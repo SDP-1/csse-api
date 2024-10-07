7cf41e6090d445404526f93fc2840e4e3621f347
Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-10-07 18:06:07 +0530

    Initial Commit

---
+++ src/main/java/com/csse/csse_api/CsseApiApplication.java
@@ -0,0 +1,13 @@
+package com.csse.csse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csse/csse_api/CsseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csse.csse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
